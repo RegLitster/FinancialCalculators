1425cf60190e340b8bc7771e7d0e5a1fb30dba6b
Reggie <reglitster00@gmail.com>
2025-09-26 15:47:10 -0400

    Almost done Commit

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -4,9 +4,41 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter what calculator you would like to do use \n 1. Mortgage Calculator \n 2. Future Value \n 3. Present Value");
+        System.out.println("Option: ");
+        int option = input.nextInt();
 
-    mortgageCal();
+        if (option == 1) {
+            mortgageCal();
+        } else if (option == 2) {
+            futureValCal();
+        } else if (option == 3) {
+            presentValueCal();
+        } else {
+            System.out.println("Invalid option. Please try again");
+        }
+    }
+    public static void presentValueCal(){
 
+    }
+    public static void futureValCal(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your deposit:");
+        double deposit = input.nextDouble();
+        System.out.println("Please enter your interest:");
+        double interest = input.nextDouble()/100;
+        System.out.println("Please enter the length of the deposit in years");
+        int lengthOfYears = input.nextInt();
+
+        //FV = P × (1 + (r / 365) )^(365 × t)
+        double interestAfter = interest/365;
+        double lengthAfter = lengthOfYears*365;
+        double futureVal = deposit * Math.pow(1+(interestAfter),lengthAfter);
+
+        System.out.printf("The future value is: $%.2f\n", futureVal);
+        double interestVal = futureVal - deposit;
+        System.out.printf("Your total interest earned is: $%.2f", interestVal);
 
     }
     public static void mortgageCal(){
@@ -14,13 +46,10 @@ public class Calculator {
 
         System.out.println("Please enter the loan amount: ");
         int principal = input.nextInt();
-
         System.out.println("Please enter the loan interest rate: ");
         double rate = input.nextDouble()/100;
-
         System.out.println("Please enter the length of the loan in years: ");
         int years = input.nextInt();
-
         //M = P × (i * (1 + i )^n / ( (1 + i)^n ) - 1
         int numbermonthlypayment = 12 * years;
 
@@ -32,14 +61,15 @@ public class Calculator {
                     decimalrate * Math.pow(1+decimalrate, numbermonthlypayment)
                     /
                         (
-                                Math.pow(1+decimalrate, numbermonthlypayment) -1 // This looks less nice that what I was trying to do but it works
+                                Math.pow(1+decimalrate, numbermonthlypayment) -1 // This looks less nice than what I was trying to do but it works
                         )
 
                 );
 
         double totalpayment = monthlypayment * numbermonthlypayment;
         double totalinterest = totalpayment - principal;
-        System.out.printf("Your loan amount is: $%.2f\n Your total interest is $%.2f " , monthlypayment,totalinterest);
+        System.out.printf("Your loan amount is: $%.2f\nYour total interest is: $%.2f\nYour total payment is: $%.2f " , monthlypayment,totalinterest,totalpayment);
+
 
 
     }
